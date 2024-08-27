a5fe18d00daad6402bd777c84a58be49576e04f8
Damesow2003 <sowgokuuza@gmail.com>
2024-08-27 12:52:19 +0000

    Gestion de la relation plusieurs a plusieurs ici cote medecin

--- projetMedecine/src/main/java/com/projetMedecine/Modele/Medecin.java
+++ projetMedecine/src/main/java/com/projetMedecine/Modele/Medecin.java
@@ -3,6 +3,9 @@ package com.projetMedecine.Modele;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "medecin")
@@ -13,4 +16,17 @@ public class Medecin {
     String specialite;
     String telephone;
 
+    @ManyToMany(
+            cascade = {
+                    CascadeType.MERGE,
+                    CascadeType.PERSIST
+            },
+            fetch = FetchType.LAZY
+    )
+    @JoinTable(
+            name="cabinet_medecin",
+            joinColumns = @JoinColumn(name="matricule"),
+            inverseJoinColumns = @JoinColumn(name = "id_cabinet")
+    )
+    private List<CabinetMedical> cabinetMedicals = new ArrayList<>();
 }
